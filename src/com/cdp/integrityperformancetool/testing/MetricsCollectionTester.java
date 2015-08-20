36417a8a80352a9c7defc7b4279c4cdcbc7088c6
bryanjrague <bryan.rague@gmail.com>
2015-08-20 16:53:14 -0400

    initial commit
    adding class for testing the MetricsColleciton class fields and methods.

---
+++ src/com/cdp/integrityperformancetool/testing/MetricsCollectionTester.java
@@ -0,0 +1,35 @@
+package com.cdp.integrityperformancetool.testing;
+
+public class MetricsCollectionTester {
+
+	
+	public static void main(String[] args) {
+		
+		/*Test Case 1
+		 * create an empty collection
+		 * add 3 imb objects to it:
+		 * 		- 1 with a Long value
+		 * 		- 1 with a String value
+		 * 		- 1 with a Float value
+		 * get the name of each
+		 * get the value of each
+		 * get the collection size
+		 * add another imb object
+		 * get the collection size
+		 * change the name of an imb
+		 */
+		
+		System.out.println(" START TEST CASE 1");
+		System.out.println("Creating test collection 1...");
+		
+		/*Test Case 2
+		 * create a collection with 100 imbs
+		 * remove 50 imbs
+		 * clear the collection
+		 * add back 100 imbs
+		 * create a new collection using the first 50 imbs in the original colleciton.
+		 * 
+		 */
+	}
+
+}
