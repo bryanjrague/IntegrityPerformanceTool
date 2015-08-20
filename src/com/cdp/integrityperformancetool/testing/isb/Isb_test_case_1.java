493b8f61bf9836de02eebdc7c1e2805ed13b4657
bryanjrague <bryan.rague@gmail.com>
2015-08-20 10:51:30 -0400

    Update to test overloaded Isb set methods
    Added tests of the int and String setter methods for all Long instance variables.

--- src/com/cdp/integrityperformancetool/testing/isb/Isb_test_case_1.java
+++ src/com/cdp/integrityperformancetool/testing/isb/Isb_test_case_1.java
@@ -5,7 +5,7 @@ import org.joda.time.DateTime;
 
 import com.cdp.integrityperformancetool.IntegrityStatisticBean;
 
-public class Isb_test_case_1 {
+public class Isb_test_case_1 extends Isb_Test_Runner{
 
 	/**
 	 * Isb_test_case_1 
@@ -14,7 +14,7 @@ public class Isb_test_case_1 {
 	 * - create a IntegrityStatisticBean object, providing no initialization arguments
 	 * - output the default values to screen for accuracy
 	 * - edit all of the ISB object's fields.
-	 * - get all of the new field values, output to screen for accuracy
+	 * - get all of the new field values, output to screen for accuracy, validate
 	 * Test Case executed on:
 	 * Test Case Notes:
 	 * 
@@ -25,45 +25,92 @@ public class Isb_test_case_1 {
 	 * @param args
 	 */
 	public Isb_test_case_1(){
-		 System.out.println("Creating ISB:...");
-	        IntegrityStatisticBean ISB_one = new IntegrityStatisticBean();
-	        printState(ISB_one);
-	        System.out.println("Editing ISB state...");
-	        ISB_one.setAverage(1213456L);
-	        ISB_one.setCount(1222L);
-	        ISB_one.setEndDate(new DateTime(2015, 8, 6, 12, 37, 40));
-	        ISB_one.setGroup("Test Group");
-	        ISB_one.setKind("Test Kind");
-	        ISB_one.setMax(999999999999L);
-	        ISB_one.setMin(0L);
-	        ISB_one.setMode("Test Mode");
-	        ISB_one.setName("Test Name");
-	        ISB_one.setStartDate(new DateTime(2015, 4, 1, 8, 0, 0));
-	        ISB_one.setSum(434343434L);
-	        ISB_one.setTotalCount(3684L);
-	        ISB_one.setUnit("Test Unit");
-	        ISB_one.setUsed("Test Used");
-	        printState(ISB_one);
+		 System.out.println("Initialized Isb_test_case_1...");
 	}
 	
-	 public void printState(IntegrityStatisticBean isb){
-	        //print current state of isb object for sanity-check and debug.
-	        System.out.println(" ** IntegrityStatisticBean State **");
-	        System.out.println(" Average: " + isb.getAverage() + " (" + isb.getAverage().getClass() + ")");
-	        System.out.println(" Count: " + isb.getCount() + " (" + isb.getCount().getClass() + ")");
-	        System.out.println(" End Date: " + isb.getEndDate() + " (" + isb.getEndDate().getClass() + ")");
-	        System.out.println(" Group: " + isb.getGroup() + " (" + isb.getGroup().getClass() + ")");
-	        System.out.println(" Kind: " + isb.getKind() + " (" + isb.getKind().getClass() + ")");
-	        System.out.println(" Max: " + isb.getMax() + " (" + isb.getMax().getClass() + ")");
-	        System.out.println(" Min: " + isb.getMin() + " (" + isb.getMin().getClass() + ")");
-	        System.out.println(" Mode: " + isb.getMode() + " (" + isb.getMode().getClass() + ")");
-	        System.out.println(" Name: " + isb.getName() + " (" + isb.getName().getClass() + ")");
-	        System.out.println(" Start Date: " + isb.getStartDate() + " (" + isb.getStartDate().getClass() + ")");
-	        System.out.println(" Sum: " + isb.getSum()  + " (" + isb.getSum().getClass() + ")");
-	        System.out.println(" Total Count: " + isb.getTotalCount() + " (" + isb.getTotalCount().getClass() + ")");
-	        System.out.println(" Unit: " + isb.getUnit() + " (" + isb.getUnit().getClass() + ")");
-	        System.out.println(" Used: " + isb.getUsed() + " (" + isb.getUsed().getClass() + ")");
-	        System.out.println(" ***********************************\n");
-	    }
+	public String execute(){
+		System.out.println("Creating Isb object with default constructor...");
+		IntegrityStatisticBean isb_1 = new IntegrityStatisticBean();
+		printState(isb_1);
+		System.out.println("Validating default values against expected...");
+		if(!isEqual(0L, isb_1.getAverage())) { return "ERROR 1"; }
+		if(!isEqual(0L, isb_1.getCount())) { return "ERROR 2"; }
+		if(!isEqual("", isb_1.getGroup())) { return "ERROR 3"; }
+		if(!isEqual("", isb_1.getKind())) { return "ERROR 4"; }
+		if(!isEqual(0L, isb_1.getMax())) { return "ERROR 5"; }
+		if(!isEqual(0L, isb_1.getMin())) { return "ERROR 6"; }
+		if(!isEqual("", isb_1.getName())) { return "ERROR 7"; }
+		if(!isEqual(0L, isb_1.getSum())) { return "ERROR 8"; }
+		if(!isEqual(0L, isb_1.getTotalCount())) { return "ERROR 9"; }
+		if(!isEqual("", isb_1.getUnit())) { return "ERROR 10"; }
+		if(!isEqual("", isb_1.getUsed())) { return "ERROR 11"; }
+		System.out.println("VALIDATED\n");
+		
+		System.out.println("Editing Isb object state...");
+		isb_1.setAverage(23L);
+		isb_1.setCount(587L);
+		isb_1.setEndDate(new DateTime());
+		isb_1.setGroup("isb_1 group");
+		isb_1.setKind("isb_1 kind");
+		isb_1.setMax(28495L);
+		isb_1.setMin(23L);
+		isb_1.setMode("isb_1 mode");
+		isb_1.setName("isb_1 name");
+		isb_1.setStartDate(new DateTime());
+		isb_1.setSum(385749L);
+		isb_1.setTotalCount(24532L);
+		isb_1.setUnit("Ms");
+		isb_1.setUsed("isb_1 used");
+		printState(isb_1);
+		System.out.println("Validating edits...");
+		if(!isEqual(23L, isb_1.getAverage())) { return "ERROR 1"; }
+		if(!isEqual(587L, isb_1.getCount())) { return "ERROR 2"; }
+		if(!isEqual("isb_1 group", isb_1.getGroup())) { return "ERROR 3"; }
+		if(!isEqual("isb_1 kind", isb_1.getKind())) { return "ERROR 4"; }
+		if(!isEqual(28495L, isb_1.getMax())) { return "ERROR 5"; }
+		if(!isEqual(23L, isb_1.getMin())) { return "ERROR 6"; }
+		if(!isEqual("isb_1 name", isb_1.getName())) { return "ERROR 7"; }
+		if(!isEqual(385749L, isb_1.getSum())) { return "ERROR 8"; }
+		if(!isEqual(24532L, isb_1.getTotalCount())) { return "ERROR 9"; }
+		if(!isEqual("Ms", isb_1.getUnit())) { return "ERROR 10"; }
+		if(!isEqual("isb_1 used", isb_1.getUsed())) { return "ERROR 11"; }
+		System.out.println("VALIDATED\n");
+		
+		System.out.println("Editing Isb object state with ints using overloaded set methods...");
+		isb_1.setAverage(231);
+		isb_1.setCount(5871);
+		isb_1.setMax(284951);
+		isb_1.setMin(231);
+		isb_1.setSum(3857491);
+		isb_1.setTotalCount(245321);
+		printState(isb_1);
+		System.out.println("Validating edits...");
+		if(!isEqual(231L, isb_1.getAverage())) { return "ERROR 1"; }
+		if(!isEqual(5871L, isb_1.getCount())) { return "ERROR 2"; }
+		if(!isEqual(284951L, isb_1.getMax())) { return "ERROR 5"; }
+		if(!isEqual(231L, isb_1.getMin())) { return "ERROR 6"; }
+		if(!isEqual(3857491L, isb_1.getSum())) { return "ERROR 8"; }
+		if(!isEqual(245321L, isb_1.getTotalCount())) { return "ERROR 9"; }
+		System.out.println("VALIDATED\n");
+		
+		System.out.println("Editing Isb object state with Strings using overloaded set methods...");
+		isb_1.setAverage("2312");
+		isb_1.setCount("58712");
+		isb_1.setMax("2849512");
+		isb_1.setMin("2312");
+		isb_1.setSum("38574912");
+		isb_1.setTotalCount("2453212");
+		printState(isb_1);
+		System.out.println("Validating edits...");
+		if(!isEqual(2312L, isb_1.getAverage())) { return "ERROR 1"; }
+		if(!isEqual(58712L, isb_1.getCount())) { return "ERROR 2"; }
+		if(!isEqual(2849512L, isb_1.getMax())) { return "ERROR 5"; }
+		if(!isEqual(2312L, isb_1.getMin())) { return "ERROR 6"; }
+		if(!isEqual(38574912L, isb_1.getSum())) { return "ERROR 8"; }
+		if(!isEqual(2453212L, isb_1.getTotalCount())) { return "ERROR 9"; }
+		System.out.println("VALIDATED\n");
+		
+		return "PASS";
+	}
 
 }
