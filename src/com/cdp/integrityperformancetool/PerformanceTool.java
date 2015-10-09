5b19ce729688e0af2de843117e3c345642afd2f3
bryanjrague <bryan.rague@gmail.com>
2015-10-09 15:59:24 -0400

    Update PerformanceTool.java

--- src/com/cdp/integrityperformancetool/PerformanceTool.java
+++ src/com/cdp/integrityperformancetool/PerformanceTool.java
@@ -13,20 +13,38 @@ public class PerformanceTool {
 
     public static void main(String args[]){
 
+
+        //////////////////////////////////////////////
+        //// Get all statistics from the master data file
+        /////////////////////////////////////////////
         StatisticsFileReader testData02 = new StatisticsFileReader();
-        testData02.setFilePath("C:\\Users\\bryan\\IdeaProjects\\Integrity Performance Tool\\Input\\TestData_02.csv");
+        //testData02.setFilePath("C:\\Users\\bryan\\IdeaProjects\\Integrity Performance Tool\\Input\\TestData_02.csv");
+        testData02.setFilePath("C:\\Users\\USX25908\\IdeaWorkspace\\IntegrityPerformanceTool\\Input\\TestData_02.csv");
         testData02.setValueSeparator(",");
         testData02.setSkipLines(1);
 
         StatisticsLibrary masterDataLib = testData02.executeStatisticsRetrieval();
+
+        ///////////////////////////////////////////////
+        /// Get only the Trigger data from the master lib
+        ///////////////////////////////////////////////
         StatisticsCollection triggers_raw = masterDataLib.getStatisticsGroupName("Triggers");
 
+
+        ///////////////////////////////////////////////
+        /// Get only the cumulative triggers from the Collection, create new collection to store them
+        ///////////////////////////////////////////////
         StatisticsCollection triggers_cumulative = new StatisticsCollection("All Triggers - Cumulative only");
         for(IntegrityStatisticBean isb : triggers_raw.getCollection()){
             if(isb.getMode().equals("cumulative")){
                 triggers_cumulative.addToCollection(isb);
             }
         }
+
+        ////////////////////////////////////////////////
+        /// For each unique trigger in the cumulative only collection, create and store a file of all stat entries
+        ///////////////////////////////////////////////
+
         //get all unique names for cumulative trigger stats
         HashMap<String,String> cumulativeUniqueNames = triggers_cumulative.getAllUniqueNameGroupPairs();
         for(String name : cumulativeUniqueNames.keySet()){
@@ -34,7 +52,8 @@ public class PerformanceTool {
             for(IntegrityStatisticBean isb : triggers_cumulative.getCollection()){
                 if(isb.getName().equals(name)) temp_collection.addToCollection(isb);
             }
-            StringBuilder filePath = new StringBuilder("C:\\Users\\bryan\\IdeaProjects\\Integrity Performance Tool\\Output\\");
+            //StringBuilder filePath = new StringBuilder("C:\\Users\\bryan\\IdeaProjects\\Integrity Performance Tool\\Output\\");
+            StringBuilder filePath = new StringBuilder("C:\\Users\\USX25908\\IdeaWorkspace\\IntegrityPerformanceTool\\Output\\");
             filePath.append("Triggers\\");
             filePath.append("Cumulative Entries\\");
             File temp_dir = new File(filePath.toString());
@@ -43,6 +62,29 @@ public class PerformanceTool {
             filePath.append(".csv");
 
             temp_collection.writeToFile(filePath.toString());
+        }
+
+
+        //////////////////////////////////////////////////
+        /// Gather all individual cumulative trigger totals and store in single file:
+        //////////////////////////////////////////////////
+        StringBuilder currStatTotalsFile = new StringBuilder("C:\\Users\\USX25908\\IdeaWorkspace\\IntegrityPerformanceTool\\Output\\");
+        currStatTotalsFile.append("Triggers\\");
+        currStatTotalsFile.append("All Triggers - Total Results.csv");
+
+        for(String name : cumulativeUniqueNames.keySet()){
+            StringBuilder currFilePath = new StringBuilder("C:\\Users\\USX25908\\IdeaWorkspace\\IntegrityPerformanceTool\\Output\\");
+            currFilePath.append("Triggers\\");
+            currFilePath.append("Cumulative Entries\\");
+            currFilePath.append(cleanString(name) + ".csv");
+
+            StatisticsFileReader currCumulativeStatsFile = new StatisticsFileReader(currFilePath.toString());
+            StatisticsLibrary currCumulativeLibrary = currCumulativeStatsFile.executeStatisticsRetrieval();
+            StatisticsCollection currCumulativeStats = currCumulativeLibrary.getStatisticsGroupName("Triggers");
+
+            currCumulativeStats.computeAllCollectionStatistics();
+            //TODO: need ot complete new methods in StatisticsCollection to write dates correctly
+            currCumulativeStats.writeCollectionTotalsToFile(currStatTotalsFile.toString());
         }
 
     }
